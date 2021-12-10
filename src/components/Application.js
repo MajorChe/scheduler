21fe6fbe0b75e81e85fe8524bc972e527a5b52b1
MajorChe <charithnanduri03@gmail.com>
2021-12-09 22:05:58 -0500

    remove static days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,24 +46,6 @@ const appointments = [
   }
 ];
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
 export default function Application(props) {
   const [days, setDays] = useState([]);
   const[dayclick, setDay] = useState('Monday')
@@ -72,6 +54,7 @@ export default function Application(props) {
     axios.get('http://localhost:8001/api/days')
     .then(response => setDays(response.data))
   },[])
+  
   const appointmentlist = appointments.map((appointment) => {
     return <Appointment key = {appointment.id} {... appointment}/>
   })
